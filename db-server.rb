4204f2dbb966ca94c4feb005cab259690d1650cd
r-goto <https://gl.creationline.com/r-goto>
2021-03-28 22:03:47 +0900

    Unnecessary lines pruned. Most of them are defined by `bootstrap_a_node` cookbook already.

--- db-server.rb
+++ db-server.rb
@@ -4,16 +4,19 @@
 # https://docs.chef.io/policyfile/
 
 name 'db-server'
-run_list 'bootstrap_a_node::default', 'chef-client::default', 'postgresql_setup::default'
+run_list 'bootstrap_a_node::default', 'postgresql_setup::default'
 default_source :chef_server, 'https://automate.cl/organizations/first-org'
 default_source :supermarket
 cookbook 'bootstrap_a_node', '~> 2'
 cookbook 'postgresql_setup', '0.1.0'
-cookbook 'chef-client', '~> 12.3.3', :supermarket
 
 ##########
 # Attributes
 ##########
+
+# Specify CCR interval and splay
+override['chef_client']['interval'] = 43200 # 12H
+override['chef_client']['splay'] = 21600 # 6H
 
 # Specify Policy name & Policy group
 override['bootstrap_a_node']['policy_name'] = 'db-server'
@@ -24,10 +27,6 @@ override['bootstrap_a_node']['chef_client']['version'] = '16.10'
 
 # Disable Slack Notification that's set only for the initial CCR
 override['chef_client']['handler']['slack']['enabled'] = false
-
-# Specify CCR interval and splay
-override['chef_client']['interval'] = 43200 # 12H
-override['chef_client']['splay'] = 21600 # 6H
 
 # Specify node Time Zone
 override['bootstrap_a_node']['timezone'] = 'Asia/Tokyo'
