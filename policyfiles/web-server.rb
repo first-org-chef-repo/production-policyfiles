19af5bb52a0ff6f8ff058da7abe75900a9e183ad
Ryuji Goto <g.ryuji0510@gmail.com>
2021-04-15 00:44:57 +0900

    Update web-server.rb

--- policyfiles/web-server.rb
+++ policyfiles/web-server.rb
@@ -15,8 +15,8 @@ cookbook 'ssh_setup', '2.0.0'
 ##########
 
 # Specify CCR interval and splay
-override['chef_client']['interval'] = 0 #86400 # 24H
-override['chef_client']['splay'] = 0 # 1H
+override['chef_client']['interval'] = 86400 # 24H
+override['chef_client']['splay'] = 1H
 
 # Specify Policy name & Policy group
 override['bootstrap_a_node']['policy_name'] = 'web-server'
